e0909324bf2ebf4eaf4f167119b9a39210734988
Joseph Nyaga <joseph.kinyua@student.moringaschool.com>
2021-06-21 23:12:49 +0300

    add the effect of the jQuery to see the toggle of project name

--- js/script.js
+++ js/script.js
@@ -13,3 +13,29 @@ $(document).ready(function() {
     });
 
 });
+$(document).ready(function() {
+    $("#wek1").hover(function() {
+        $("#port1").toggle();
+    });
+    $("#wek2").hover(function() {
+        $("#port2").toggle();
+    });
+    $("#wek3").hover(function() {
+        $("#port3").toggle();
+    });
+    $("#wek4").hover(function() {
+        $("#port4").toggle();
+    });
+    $("#wek5").hover(function() {
+        $("#port5").toggle();
+    });
+    $("#wek6").hover(function() {
+        $("#port6").toggle();
+    });
+    $("#wek7").hover(function() {
+        $("#port7").toggle();
+    });
+    $("#wek8").hover(function() {
+        $("#port8").toggle();
+    });
+});
