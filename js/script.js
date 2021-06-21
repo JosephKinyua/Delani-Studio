3cf473a5d4a5d297c27a1baa9ae77330fc7fb580
Joseph Nyaga <joseph.kinyua@student.moringaschool.com>
2021-06-21 23:09:45 +0300

    styling of h3

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(".image1").click(function() {
+        $("#pic1").slideToggle();
+        $("#boom1").slideToggle();
+    });
+    $(".image2").click(function() {
+        $("#pic2").slideToggle();
+        $("#boom2").slideToggle();
+    });
+    $(".image3").click(function() {
+        $("#pic3").slideToggle();
+        $("#boom3").slideToggle();
+    });
+
+});
